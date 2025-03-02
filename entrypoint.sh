98bdb00eddacb93aaf62ca141c670135b1adb5c7
Eric Ortega <24722023+ejortega@users.noreply.github.com>
2025-03-02 10:44:38 -0600

    Fix build on macOS

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,15 @@ set -e
 current_uid=$(id -u user)
 current_gid=$(id -g user)
 
-if [ "$LOCAL_UID" != "$current_uid" ]; then
-    echo "Updating UID from $current_uid to $LOCAL_UID"
-    usermod -u $LOCAL_UID user
-fi
+if [ "$$" -ne 1 ]; then
+    if [ "$LOCAL_UID" != "$current_uid" ]; then
+        usermod -u $LOCAL_UID user
+    fi
 
-if [ "$LOCAL_GID" != "$current_gid" ]; then
-    echo "Updating GID from $current_gid to $LOCAL_GID"
-    groupmod -g $LOCAL_GID user
+    if [ "$LOCAL_GID" != "$current_gid" ]; then
+        groupmod -g $LOCAL_GID user
+    fi
 fi
-
-# Correct ownership if necessary (e.g., after ID changes)
-chown -R user:user /home/user
 
 # Execute the passed command
 exec "$@"
